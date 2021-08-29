53296a8f3f9206e456ed07109c620cb5d4a22d7a
Rahul Jain <rahulj06.rj@gmail.com>
2021-08-29 14:22:43 +0530

    fuctional_component

---
+++ confusion/App/src/components/MainComponent.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from './MenuComponent';
+import { DISHES } from '../shared/dishes';
+import DishDetail from './DishdetailComponent';
+
+class Main extends Component{
+
+  constructor(props){
+    super(props);
+
+    console.log(props);
+
+    this.state = {
+      dishes: DISHES,
+      selectedDish: null
+    };
+
+  } 
+
+  onDishSelect(dishId){
+    this.setState({selectedDish: dishId});
+  }
+
+
+  render(){
+
+    return(
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+              <NavbarBrand href="/"> Ristorante Con Fusion </NavbarBrand>
+          </div>
+        </Navbar>
+
+        <div className="container">
+          <div className="row">
+          <Menu dishes={ this.state.dishes } 
+            onClick={(dishId) => this.onDishSelect(dishId)}/>
+          </div>
+          <div className="row">
+          <DishDetail 
+            dish={this.state.dishes.filter((dish)=>dish.id===this.state.selectedDish )[0]} />
+          </div>
+      </div>
+      </div>
+    );
+
+  }
+
+}
+
+export default Main;
